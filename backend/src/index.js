ca5304902ac1b1a847a1663cdd294e741ea63bac
NavvAbhishek <abi2000navi@gmail.com>
2025-03-26 02:51:33 +0530

    impliment signup

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,8 @@ import { connectDB } from "./lib/db.js"
 const app = express()
 
 const PORT = process.env.PORT
+//? Allow you to extract the JSON data from body
+app.use (express.json()) 
 
 app.use("/api/auth", authRoutes)
 
