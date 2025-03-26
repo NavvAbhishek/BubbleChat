3ef3118d4623e3b435f4f42b926a7322caf01039
NavvAbhishek <abi2000navi@gmail.com>
2025-03-26 18:40:32 +0530

    impliment middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from 'dotenv'
 dotenv.config()
+import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.route.js"
 import { connectDB } from "./lib/db.js"
@@ -10,6 +11,8 @@ const app = express()
 const PORT = process.env.PORT
 //? Allow you to extract the JSON data from body
 app.use (express.json()) 
+//? Allow you to parse the cookies
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 
