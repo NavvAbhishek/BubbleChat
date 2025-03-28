b66f9f58ba18e0c31143c3bb823cd66b7b5abb31
NavvAbhishek <abi2000navi@gmail.com>
2025-03-28 14:47:15 +0530

    implement auth state management with zustand

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
@@ -6,8 +6,17 @@ import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import SettingsPage from "./pages/SettingsPage";
+import { useAuthStore } from "./store/useAuthStore";
 
 const App = () => {
+  const {authUser, checkAuth} = useAuthStore()
+
+  useEffect(()=>{
+    checkAuth()
+  },[checkAuth])
+
+  console.log({authUser})
+  
   return (
     <div>
       <Navbar />
