41bb794d1b6b41400e6f25cb4b54c61165c6ef83
NavvAbhishek <abi2000navi@gmail.com>
2025-03-28 14:16:24 +0530

    setup frontend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
 
 const App = () => {
   return (
-    <div>Hello!</div>
-  )
-}
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<ProfilePage />} />
+        <Route path="/profile" element={<SettingsPage />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
