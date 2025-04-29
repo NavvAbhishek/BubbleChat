e09acfa57d4cc2ca3b7412e84255a87a78b908cc
NavvAbhishek <abi2000navi@gmail.com>
2025-04-29 17:47:19 +0530

    impliment chat header

---
+++ frontend/src/components/ChatContainer.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import ChatHeader from "./ChatHeader";
+
+const ChatContainer = () => {
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChatStore();
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
+
+  if (isMessagesLoading) return <div>Loading...</div>;
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <p>messages...</p>
+    </div>
+  );
+};
+
+export default ChatContainer;
