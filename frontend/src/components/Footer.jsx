adc24b0db5ad08be0378b90a0209001a01109964
NavvAbhishek <abi2000navi@gmail.com>
2025-05-17 03:28:19 +0530

    add footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <div className="bg-deepBlue w-screen py-2 fixed bottom-0 left-0">
+      <span className="text-white">Design and Developed by</span>{" "}
+      <a
+        href="https://www.linkedin.com/in/navabhishek/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-lightYellow"
+      >
+        Navindu Abhishek
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import heroImg from "../assets/hero_img.jpg";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
     <div className="bg-white h-[calc(100vh-64px)] text-black flex flex-col justify-center items-center text-center px-6">
       <h1 className="text-4xl font-bold mb-8">
         Welcome to{" "}
-        <span className=" bg-lightYellow px-2 py-1 rounded-lg text-blue">Bubble Chat</span>
+        <span className=" bg-lightYellow px-2 py-1 rounded-lg text-blue">
+          Bubble Chat
+        </span>
       </h1>
       <img src={heroImg} className="h-80 rounded-xl" alt="hero_image" />
       <p className="text-md my-6">
@@ -19,9 +22,10 @@ const HomePage = () => {
         <div className="text-lg font-semibold">Send Images</div>
         <div className="text-lg font-semibold">Secure & Private</div>
       </div>
-      <button className="bg-lightYellow text-deepBlue text-lg px-6 py-2 rounded-lg mt-4 cursor-pointer font-bold">
+      <button className="bg-lightYellow text-deepBlue text-lg px-6 py-2 rounded-lg my-4 cursor-pointer font-bold">
         <Link to={"/chat"}>Lets chat</Link>
       </button>
+      <Footer />
     </div>
   );
 };
