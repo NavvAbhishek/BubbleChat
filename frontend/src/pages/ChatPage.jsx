53aa281003af8607398be65bf86085df7f2fcd49
NavvAbhishek <abi2000navi@gmail.com>
2025-07-01 23:13:23 +0530

    update chatpage UI

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -6,7 +6,7 @@ import ChatContainer from "../components/ChatContainer";
 const ChatPage = () => {
   const { selectedUser } = useChatStore();
   return (
-    <div className="h-screen bg-base-200">
+    <div className="h-[calc(100vh-64px)] bg-base-200">
       <div className="flex items-center justify-center pt-12 px-4">
         <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl h-[calc(100vh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
