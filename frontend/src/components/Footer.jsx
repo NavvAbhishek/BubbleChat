9e8345c715c220b1bef3dbd93649d11b6fc00e88
NavvAbhishek <abi2000navi@gmail.com>
2025-07-11 17:04:04 +0530

    fix footer alignment

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="bg-deepBlue w-screen py-2 fixed bottom-0 left-0">
+    <div className="bg-deepBlue w-screen py-2 fixed bottom-0 left-0 flex gap-1 justify-center">
       <span className="text-white">Design and Developed by</span>{" "}
       <a
         href="https://www.linkedin.com/in/navabhishek/"
